e854e1c31a8519eaf3b3a2e87e80ac7e0d7b6785
pHoeniX-svg <vansomecsam@gmail.com>
2022-04-29 16:07:33 +0100

    redux dev tools added

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
-import * as actions from './actions';
 import store from './store';
 
 store.subscribe(() => {
   console.log('Store Changed');
 });
 
-store.dispatch(actions.addBug('Bug 1'));
-store.dispatch(actions.addBug('Bug 2'));
-store.dispatch(actions.addBug('Bug 3'));
-store.dispatch(actions.resolveBug(1));
+// store.dispatch(actions.addBug('Bug 1'));
+// store.dispatch(actions.addBug('Bug 2'));
+// store.dispatch(actions.addBug('Bug 3'));
+// store.dispatch(actions.resolveBug(1));
 
 console.log(store.getState());
+
+// declare global {
+//   interface Window {
+//     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+//   }
+// }
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducer, composeEnhancers());
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,6 @@
+import { devToolsEnhancer } from '@redux-devtools/extension';
 import { legacy_createStore as createStore } from 'redux';
 import { reducer } from './reducer';
 
-const store = createStore(
-  reducer,
-  // @ts-ignore
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(reducer, devToolsEnhancer({ trace: true }));
 export default store;
